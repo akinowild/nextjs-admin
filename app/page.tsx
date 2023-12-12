a38f3a8faa37a651c7aa8e13ba6f2d61493c31d5
秋野 <harky@vip.qq.com>
2023-12-12 18:26:20 +0800

    feat: 安装sass集成在里面

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 import Cover from './compoments/cover/page'
 import {useState} from "react";
+import './assets/css/index.scss'
 
 export default function Page() {
     const [number=false,setNumber] = useState()
-  return <main className="flex min-h-screen flex-col p-6">
-    <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <Cover />
-      </div>
-  </main>;
+    return <main className="flex min-h-screen flex-col p-6">
+        <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+            <Cover />
+        </div>
+    </main>;
 }
